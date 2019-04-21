e6530b4c70f65e4d9b395a06b95c6819000eaabf
Ion D. Filho <iondrimba@gmail.com>
2019-04-21 18:21:58 -0300

    simple cube demo

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,14 @@
+import '@babel/polyfill';
 import '../scss/demo.scss';
+// import'./threex/threex-artoolkitsource';
+// import'./threex/threex-artoolkitcontext';
+// import'./jsartoolkit5/artoolkit.min';
+// import'./jsartoolkit5/artoolkit.api';
+// import'./threex/threex-arbasecontrols';
+// import'./threex/threex-armarkercontrols';
 
 export default class App {
-  constructor() {
-    this.init();
-  }
-
-  init() {
+  async init() {
     this.star = new THREE.Object3D();
     this.house = new THREE.Object3D();
 
@@ -13,65 +16,102 @@ export default class App {
     this.createCamera();
     this.addAmbientLight();
     this.addSpotLight();
-
     this.addCameraControls();
     this.addFloor();
 
+    // this.spaceShip = await this.loadModelsAsync('dinosaur');
+    // this.spaceShip.scale.set(7, 7, 7);
+
+    // this.dinosaur = await this.loadModelsAsync('dinosaur');
+    // this.dinosaur.scale.set(5, 5, 5);
+    // this.dinosaur.rotateY(this.radians(180));
+    // this.dinosaur.position.set(15, 0, 15);
+    // this.scene.add(this.dinosaur);
+
+    // this.tower = await this.loadModelsAsync('tower');
+    // this.tower.scale.set(5, 5, 5);
+    // this.tower.rotateY(this.radians(180));
+    // this.tower.position.set(-30, 0, -30);
+    // this.scene.add(this.tower);
+
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+    this.cube = new THREE.Mesh(geometry, material);
+    // this.cube.position.set(0, 0, 0);
+
+    this.setupARToolkitContext();
+    this.setupARToolkitSource();
+    this.setupMarkers();
     this.animate();
+  }
 
-    this.loadModels('space-ship', (spaceShip) => {
-      const scale = 7;
-      spaceShip.scale.set(scale, scale, scale);
-
-      this.scene.add(spaceShip);
+  setupARToolkitContext() {
+    this.arToolkitContext = new THREEx.ArToolkitContext({
+      cameraParametersUrl: './data/camera_para.dat',
+      detectionMode: 'mono'
     });
 
-    this.loadModels('dinosaur', (dinosaur) => {
-      const scale = 5;
-
-      dinosaur.scale.set(scale, scale, scale);
-      dinosaur.rotateY(this.radians(180));
-      dinosaur.position.set(15, 0, 15);
-
-      this.scene.add(dinosaur);
-    });
-
-    this.loadModels('tower', (tower) => {
-      const scale = 5;
-
-      tower.scale.set(scale, scale, scale);
-      tower.rotateY(this.radians(180));
-      tower.position.set(-30, 0, -30);
-
-      this.scene.add(tower);
+    this.arToolkitContext.init(() => {
+      this.camera.projectionMatrix.copy(this.arToolkitContext.getProjectionMatrix());
     });
   }
 
-  loadModels(name, callback) {
-    const mtlLoader = new THREE.MTLLoader();
-    const folder = 'models/';
-
-    mtlLoader.setPath(folder);
-    mtlLoader.load(`${name}.mtl`, (materials) => {
-      materials.preload();
-
-      const objLoader = new THREE.OBJLoader();
-
-      objLoader.setMaterials(materials);
-      objLoader.setPath(folder);
-
-      objLoader.load(`${name}.obj`, (object) => {
-        object.castShadow = true;
-
-        callback(object);
-      });
+  setupARToolkitSource() {
+    this.arToolkitSource = new THREEx.ArToolkitSource({
+      sourceType: 'webcam',
     });
+
+    this.arToolkitSource.init(() => {
+      this.onResize();
+    });
+  }
+
+  setupMarkers() {
+    const patternArray = [{
+      id: 'letterA',
+      model: this.cube
+    }];
+
+    patternArray.map((pattern) => {
+      const markerRoot = new THREE.Group();
+      this.scene.add(markerRoot);
+
+      new THREEx.ArMarkerControls(this.arToolkitContext, markerRoot, {
+        type: 'pattern', patternUrl: `./data/${pattern.id}.patt`,
+      });
+
+      markerRoot.add(pattern.model);
+    });
+  }
+
+  loadModelsAsync(name) {
+    return new Promise((resolve) => {
+      const mtlLoader = new THREE.MTLLoader();
+      const folder = 'models/';
+
+      mtlLoader.setPath(folder);
+      mtlLoader.load(`${name}.mtl`, (materials) => {
+        materials.preload();
+
+        const objLoader = new THREE.OBJLoader();
+
+        objLoader.setMaterials(materials);
+        objLoader.setPath(folder);
+
+        objLoader.load(`${name}.obj`, (object) => {
+          object.castShadow = true;
+
+          resolve(object);
+        });
+      });
+    })
   }
 
   createScene() {
     this.scene = new THREE.Scene();
 
-    this.renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+    this.renderer = new THREE.WebGLRenderer({ alpha: true});
+    this.renderer.setClearColor( 0x000000, 0 ); // the default
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
     this.renderer.shadowMap.enabled = true;
@@ -81,7 +121,7 @@ export default class App {
   }
 
   createCamera() {
-    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight);
+    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
     this.camera.position.set(-5, 19, 32);
 
     this.scene.add(this.camera);
@@ -110,6 +150,13 @@ export default class App {
     this.camera.aspect = ww / wh;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(ww, wh);
+
+    this.arToolkitSource.onResize();
+    this.arToolkitSource.copyElementSizeTo(this.renderer.domElement);
+
+    if (this.arToolkitContext.arController) {
+      this.arToolkitSource.copyElementSizeTo(this.arToolkitContext.arController.canvas)
+    }
   }
 
   addFloor() {
@@ -123,10 +170,6 @@ export default class App {
     plane.receiveShadow = true;
 
     this.scene.add(plane);
-  }
-
-  rotateObject(group, value) {
-    group.rotation.y += value;
   }
 
   addSpotLight() {
@@ -148,6 +191,10 @@ export default class App {
     this.controls.update();
 
     this.renderer.render(this.scene, this.camera);
+
+    if (this.arToolkitSource && this.arToolkitSource.ready) {
+      this.arToolkitContext.update(this.arToolkitSource.domElement);
+    }
 
     requestAnimationFrame(this.animate.bind(this));
   }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin =  require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new CopyPlugin([
+      { from: 'source', to: 'dest' },
+      { from: 'other', to: 'public' },
+    ]),
+  ],
+};
 
 module.exports = merge(common, {
   mode: 'production',
@@ -17,6 +27,12 @@ module.exports = merge(common, {
     ]
   },
   plugins: [
+    new CopyPlugin([
+      { from: './src/scripts/ar.js', to: 'scripts' },
+      { from: './src/data', to: 'data' },
+      { from: './src/models', to: 'models' },
+      { from: './src/markers', to: 'markers' },
+    ]),
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
       chunkFilename: '[id].[hash].css'
